6563d86036da8eacf6a6609f26112f118370e5fc
storrowking <iam_calvin@icloud.com>
2014-10-30 13:36:21 -0400

    Added Step by Step Fibonacci Sequence

--- src/FibonacciRecursion.java
+++ src/FibonacciRecursion.java
@@ -13,7 +13,7 @@ public class FibonacciRecursion {
     public static void main(String[] args) {
 
         // prints fibonacci numbers up to specified number
-        System.out.println("Enter number for Fibonacci to print:");
+        System.out.print("Enter number for Fibonacci to print:");
         int userInput = new Scanner(System.in).nextInt();
 
         System.out.println("Fibonacci series up to " + userInput + " numbers :");
@@ -21,6 +21,10 @@ public class FibonacciRecursion {
         for (int i = 1; i < userInput; i++) {
             System.out.print(fibonacci(i) + " ");
         }
+        System.out.println("\n");
+
+        // prints fibonacci step by step sequence
+        System.out.println("\n" + fibonacci2(userInput));
 
         /*
         // prints out entire fibonacci sequence
@@ -43,5 +47,31 @@ public class FibonacciRecursion {
         } else {
            return fibonacci(n - 1) + fibonacci(n - 2); // recursion
         }
+    }
+
+    // fibonacci that prints the step by step sequence
+    public static long fibonacci2(int n)  {
+
+        long number1;
+        long number2;
+        long result;
+
+        if(n == 0) {
+            return 0;
+
+        } else if(n == 1) {
+            return 1;
+
+        } else {
+            number1 = fibonacci2(n - 1);
+            number2 = fibonacci2(n - 2);
+
+            result = number1 + number2;
+
+            System.out.println("Fibonacci2(n - 1) = " + number1);
+            System.out.println("Fibonacci2(n - 2) = " + number2);
+            System.out.println("\t\t   Result = " + result + "\n");
+        }
+        return result;
     }
 } // end of class
